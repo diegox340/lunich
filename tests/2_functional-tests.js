83382ab9729e311fde995e1c39e9ed9d2c9f4d2e
diegox340 <jorgediegocofre@gmail.com>
2025-09-24 16:24:38 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -85,17 +85,17 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      browser.visit('/', function () {
-        browser.fill('surname', 'Colombo');
-        browser.pressButton('submit', function () {
-          browser.assert.success();
-          browser.assert.text('span#name', 'Cristoforo');
-          browser.assert.text('span#surname', 'Colombo');
-          browser.assert.elements('span#dates', 1);
-          done();
-        });
-      });
+  browser.visit('/', function () {
+    browser.fill('surname', 'Colombo');
+    browser.pressButton('submit', function () {
+      browser.assert.success();
+      browser.assert.text('span#name', 'Cristoforo');
+      browser.assert.text('span#surname', 'Colombo');
+      browser.assert.elements('span#dates', 1);
+      done();
     });
+  });
+});
 
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
